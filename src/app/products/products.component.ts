10773088a0f37f114639f7ca3490467da398aadd
christanshane <cosmotechnologist@yahoo.com>
2018-02-19 01:31:53 +0800

    listing now with images v0.4

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../product-model';
-import { Upload } from '../services/upload.service';
 
 @Component({
   selector: 'app-products',
